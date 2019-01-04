3b327de51b06fcb25cfed9ee72c37ae18f34bdf6
fandiar <fandi@gredu.asia>
2019-01-04 15:01:22 +0700

    research & implement sorting table field in page Daftar Murid

--- src/views/beranda/beranda.js
+++ src/views/beranda/beranda.js
@@ -1,16 +1,61 @@
 import React, { Component } from 'react'
+import Axios from 'axios'
 import './../../styles/beranda.css'
 
 import Header from '../global/header'
 import MenuBar from '../global/navbar'
 
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
+import '../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css'
+
 class Beranda extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            users: [],
+            error: null
+        };
+        this.onSortChange = this.onSortChange.bind(this);
+    }
+
+    onSortChange(sortName, sortOrder) {
+        console.info('onSortChange', arguments);
+        this.setState({
+            sortName,
+            sortOrder
+        });
+    }
+
+    componentDidMount() {
+        const url = 'https://jsonplaceholder.typicode.com/users'
+        Axios.get(url)
+            .then(res => {
+                const users = res.data;
+                this.setState({ users });
+                console.log('DATA', res);
+            })
+    }
+
     render() {
+        const options = {
+            sortName: this.state.sortName,
+            sortOrder: this.state.sortOrder,
+            onSortChange: this.onSortChange
+        };
         return (
             <div className="beranda">
                 <Header></Header>
                 <MenuBar></MenuBar>
+                <BootstrapTable data={this.state.users} options={options}>
+                    <TableHeaderColumn dataField='id' isKey dataSort>Product ID</TableHeaderColumn>
+                    <TableHeaderColumn dataField='name' dataSort>Product Name</TableHeaderColumn>
+                    <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+                </BootstrapTable>
             </div>
+
+
+
         )
     }
 }
--- src/views/murid/murid.js
+++ src/views/murid/murid.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import Axios from 'axios';
-import './../../styles/murid.css';
+import Axios from 'axios'
+import './../../styles/murid.css'
 import './../../styles/global/component.css'
-import { Table } from 'reactstrap';
+// import { Table } from 'reactstrap'
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
+import '../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css'
 
-import Header from '../global/header';
-import MenuBar from '../global/navbar';
+import Header from '../global/header'
+import MenuBar from '../global/navbar'
 
 class DaftarMurid extends Component {
     constructor(props) {
@@ -17,15 +19,23 @@ class DaftarMurid extends Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.onSortChange = this.onSortChange.bind(this);
+    }
+
+    onSortChange(sortName, sortOrder) {
+        console.info('onSortChange', arguments);
+        this.setState({
+            sortName,
+            sortOrder
+        });
     }
 
     componentDidMount() {
-        const url = 'https://jsonplaceholder.typicode.com/users'
-        Axios.get(url)
+        Axios.get(process.env.API_URL + '/users')
             .then(res => {
                 const users = res.data;
                 this.setState({ users });
-                console.log('DATA', res);
+                
             })
     }
 
@@ -37,6 +47,11 @@ class DaftarMurid extends Component {
     }
 
     render() {
+        const options = {
+            sortName: this.state.sortName,
+            sortOrder: this.state.sortOrder,
+            onSortChange: this.onSortChange
+        };
         return (
             <div className="murid">
                 <Header></Header>
@@ -48,33 +63,14 @@ class DaftarMurid extends Component {
                             <h5><strong>Daftar Murid Kelas X IPA 2</strong></h5>
                             <h6>Tahun Ajaran 2018/2019</h6>
                             <br />
-                            <Table bordered striped responsive hover className="table-sm">
-                                <thead>
-                                    <tr>
-                                        <th>NIS</th>
-                                        <th>NISN</th>
-                                        <th>Nama Murid</th>
-                                        <th>Kehadiran Rata-Rata</th>
-                                        <th>Nilai Rata-Rata</th>
-                                        <th>Peringkat</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        this.state.users.map(function (i, x) {
-                                            console.log('I', i);
-                                            return <tr>
-                                                <th scope="row">{i.name}</th>
-                                                <td>{i.username}</td>
-                                                <td>{i.email}</td>
-                                                <td>{i.address.city}</td>
-                                                <td>{i.address.street}</td>
-                                                <td>{i.address.zipcode}</td>
-                                            </tr>
-                                        })
-                                    }
-                                </tbody>
-                            </Table>
+                            <BootstrapTable data={this.state.users} options={options}>
+                                <TableHeaderColumn dataField='id' isKey dataSort>NIS</TableHeaderColumn>
+                                <TableHeaderColumn dataField='email' dataSort>NISN</TableHeaderColumn>
+                                <TableHeaderColumn dataField='username' dataSort>Nama Murid</TableHeaderColumn>
+                                <TableHeaderColumn dataField='username' dataSort>Kehadiran Rata-Rata</TableHeaderColumn>
+                                <TableHeaderColumn dataField='username' dataSort>Nilai Rata-Rata</TableHeaderColumn>
+                                <TableHeaderColumn dataField='username' dataSort>Peringkat</TableHeaderColumn>
+                            </BootstrapTable>
                         </div>
                     </div>
                 </div>
