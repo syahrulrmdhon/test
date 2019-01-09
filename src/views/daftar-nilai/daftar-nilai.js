2a4950b8505097e599e3e0d4900b65a635ae1faf
Fandi A Riyanto <fandi@gredu.asia>
2019-01-09 17:14:58 +0700

    refactor page Daftar Nilai

--- src/views/daftar-nilai/daftar-nilai.js
+++ src/views/daftar-nilai/daftar-nilai.js
@@ -6,38 +6,17 @@ import MenuBar from '../global/navbar'
 
 import { Table, TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap'
 import classnames from 'classnames';
-
-const listClass = [
-    { label: "X IPA 1", value: "x ipa 1" },
-    { label: "X IPA 2", value: "x ipa 2" },
-    { label: "X IPS 1", value: "x ips 1" },
-    { label: "X IPS 2", value: "x ips 2" }
-];
-const listSemester = [
-    { label: "1", value: 1 },
-    { label: "2", value: 2 }
-]
-const listPelajaran = [
-    { label: "Fisika Dasar II", value: "fisika2" },
-    { label: "Matematika Dasar II", value: "matematika2" },
-    { label: "Sejarah Nasional II", value: "sejarah2" },
-    { label: "Biologi II", value: "biologi2" }
-];
+import FilterNilai from './filter';
 
 class DaftarNilai extends Component {
     constructor(props) {
         super(props);
 
-        this.toggle = this.toggle.bind(this);
         this.state = {
-            activeTab: '1',
-            listClass,
-            selectedClass: "",
-            listSemester,
-            selectedSemester: "",
-            listPelajaran,
-            selectedPelajaran: ""
+            activeTab: '1'
         };
+
+        this.toggle = this.toggle.bind(this);
     }
 
     toggle(tab) {
@@ -56,47 +35,7 @@ class DaftarNilai extends Component {
                 <div className="content">
                     <div className="row">
                         <div className="left-content col-2">
-                            <h5><strong>Filter</strong></h5>
-                            <br />
-                            <form onSubmit={this.handleSubmit}>
-                                <label>Semester</label>
-                                <select value={this.state.selectedSemester}
-                                    onChange={(e) => this.setState({ selectedSemester: e.target.value })}>
-                                    {
-                                        this.state.listSemester.map((semester) =>
-                                            <option key={semester.value} value={semester.value}>
-                                                {semester.label}
-                                            </option>
-                                        )
-                                    }
-                                </select>
-                                <br /><br />
-                                <label>Kelas</label>
-                                <select value={this.state.selectedClass}
-                                    onChange={(e) => this.setState({ selectedClass: e.target.value })}>
-                                    {
-                                        this.state.listClass.map((kelas) =>
-                                            <option key={kelas.value} value={kelas.value}>
-                                                {kelas.label}
-                                            </option>
-                                        )
-                                    }
-                                </select>
-                                <br /><br />
-                                <label>Mata Pelajaran</label>
-                                <select value={this.state.selectedPelajaran}
-                                    onChange={(e) => this.setState({ selectedPelajaran: e.target.value })}>
-                                    {
-                                        this.state.listPelajaran.map((pelajaran) =>
-                                            <option key={pelajaran.value} value={pelajaran.value}>
-                                                {pelajaran.label}
-                                            </option>
-                                        )
-                                    }
-                                </select>
-                                <br /><br />
-                                <button type="submit" className="btn-green">Filter</button>
-                            </form>
+                            <FilterNilai />
                         </div>
                         <div className="right-content col-10">
                             <div className="row">
