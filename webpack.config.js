e443599885308e37b4763d51f373376911dc31b2
hengkyreza7 <hengkyreza7@gmail.com>
2019-01-10 13:22:55 +0700

    scss loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
+
+
 const API_URL = {
     production: JSON.stringify('https://jsonplaceholder.typicode.com'),
     development: JSON.stringify('https://jsonplaceholder.typicode.com/development')
 }
+
 const webpack = require('webpack');
-// var ROOT = path.resolve(__dirname, '..');
-// var root = path.join.bind(path, ROOT);
+var ROOT = path.resolve(__dirname, '..');
+var root = path.join.bind(path, ROOT);
 
 
 module.exports = (env) => ({
     devServer: {
         historyApiFallback: true,
         contentBase: path.join(__dirname, 'build'),
-        compress: true,
-        port: 8080
+        compress: true
     },
     devtool: 'source-map',
     output: {
@@ -35,8 +38,24 @@ module.exports = (env) => ({
             },
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ],
 
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader",
+                    options: {
+                        includePaths: ["./src/styles/"]
+                    }
+                }]
             },
             {
                 test: /\.(ttf|woff|eot|png|svg|pdf|woff(2)?)(\?[a-z0-9=&.]+)?$/,
@@ -50,17 +69,20 @@ module.exports = (env) => ({
             template: "./public/index.html",
             filename: "./index.html"
         }),
+     
         new MiniCssExtractPlugin({
             filename: "[name].css",
             chunkFilename: "[id].css"
         }),
         new webpack.DefinePlugin({
             'process.env': {
-                'API_URL': API_URL[env.TARGET_ENV]
+                'API_U RL': API_URL[env.TARGET_ENV]
             }
-        })
+        }),
+        // new ExtractTextPlugin('./src/styles/tes.scss'),
+
 
     ]
-  
+
 
 });
