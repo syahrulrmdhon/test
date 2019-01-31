bbb4f96f9c6da53213c4bd41c54be84ca6404031
Fandi A Riyanto <fandi@gredu.asia>
2019-01-31 13:40:30 +0700

    defaut layout when data not yet load page rapor

--- src/views/rapor/rapor.js
+++ src/views/rapor/rapor.js
@@ -1,17 +1,18 @@
-import React, { Component } from "react"
-import "./../../styles/rapor.css"
-import "./../../styles/global/component.css"
+import React, { Component } from "react";
+import "./../../styles/rapor.css";
+import "./../../styles/global/component.css";
 
-import { TabContent, TabPane, Nav, NavItem, NavLink } from "reactstrap"
-import { Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap'
-import classnames from "classnames"
+import { TabContent, TabPane, Nav, NavItem, NavLink } from "reactstrap";
+import { Button, ButtonGroup, ButtonToolbar } from "react-bootstrap";
+import classnames from "classnames";
 
-import Header from "../global/header"
-import FilterRapor from "./filter"
-import TablePengetahuan from "./table-pengetahuan"
-import TableKeterampilan from "./table-keterampilan"
-import TableSikap from "./table-sikap"
-import { apiClient } from "../../utils/apiClient"
+import Header from "../global/header";
+import FilterRapor from "./filter";
+import TablePengetahuan from "./table-pengetahuan";
+import TableKeterampilan from "./table-keterampilan";
+import TableSikap from "./table-sikap";
+import { apiClient } from "../../utils/apiClient";
+import { NotAvailable } from "../../views/global/notAvailable";
 
 export default class Rapor extends Component {
   constructor(props) {
@@ -32,7 +33,7 @@ export default class Rapor extends Component {
       dTableKnowledge: [],
       dTableSkill: [],
       dTableAttitude: [],
-      semesterActive: ''
+      semesterActive: ""
     };
     this.toggle = this.toggle.bind(this);
     this.getSemesterList = this.getSemesterList.bind(this);
@@ -40,14 +41,15 @@ export default class Rapor extends Component {
     this.getStatusList = this.getStatusList.bind(this);
     this.onChangeStatus = this.onChangeStatus.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.nameClicked = this.nameClicked.bind(this)
-    this.getActiveSemester = this.getActiveSemester.bind(this)
+    this.nameClicked = this.nameClicked.bind(this);
+    this.getActiveSemester = this.getActiveSemester.bind(this);
   }
   componentDidMount() {
+    this.notKnowledge();
     this.getSemesterList();
     this.getStatusList();
-    this.getClassName()
-    this.getActiveSemester()
+    this.getClassName();
+    this.getActiveSemester();
   }
   toggle(tab) {
     if (this.state.activeTab !== tab) {
@@ -57,19 +59,19 @@ export default class Rapor extends Component {
     }
   }
   getActiveSemester() {
-    const periodObject = localStorage.getItem("current_period")
-    const periodParse = JSON.parse(periodObject)
-    const activeSemester = periodParse.period_name
-    this.state.semesterActive = activeSemester
-    this.getMountData()
+    const periodObject = localStorage.getItem("current_period");
+    const periodParse = JSON.parse(periodObject);
+    const activeSemester = periodParse.period_name;
+    this.state.semesterActive = activeSemester;
+    this.getMountData();
   }
   getClassName() {
-    const classList = localStorage.getItem("homeroom_class")
-    const classes = JSON.parse(classList)
-    const nameClass = classes.name
+    const classList = localStorage.getItem("homeroom_class");
+    const classes = JSON.parse(classList);
+    const nameClass = classes.name;
     this.setState({
       nameClass: nameClass
-    })
+    });
   }
   getSemesterList() {
     const url = `v1/filters/semesters`;
@@ -77,7 +79,7 @@ export default class Rapor extends Component {
       let semester = [];
       for (var i in res.data.data) {
         const datum = res.data.data[i];
-        datum.map(function (data, i) {
+        datum.map(function(data, i) {
           semester.push({ value: data.id, label: data.period_name });
         });
       }
@@ -90,14 +92,14 @@ export default class Rapor extends Component {
     this.setState({ selectedSemester });
   }
   getStatusList() {
-    const url = `v1/filters/risk_status`
+    const url = `v1/filters/risk_status`;
     apiClient("get", url).then(res => {
       let status = [];
       for (var i in res.data.data) {
         const datum = res.data.data[i];
-        datum.map(function (data, i) {
+        datum.map(function(data, i) {
           status.push({ value: data.key, label: data.value });
-        })
+        });
       }
       this.setState({
         listStatus: status
@@ -108,28 +110,59 @@ export default class Rapor extends Component {
     this.setState({ selectedStatus });
   }
   getKnowledge() {
-    let url = `v1/scores/report?semester=${this.state.selectedSemester.label}&category=knowledge&class_id=${localStorage.getItem("class_id")}&risk_status=${this.state.selectedStatus.value}`;
+    let url = `v1/scores/report?semester=${
+      this.state.selectedSemester.label
+    }&category=knowledge&class_id=${localStorage.getItem(
+      "class_id"
+    )}&risk_status=${this.state.selectedStatus.value}`;
     return apiClient("get", url);
   }
   getMountKnowledge() {
-    let url = `v1/scores/report?semester=${this.state.semesterActive}&category=knowledge&class_id=${localStorage.getItem("class_id")}&risk_status=all`;
-    return apiClient('get', url)
+    let url = `v1/scores/report?semester=${
+      this.state.semesterActive
+    }&category=knowledge&class_id=${localStorage.getItem(
+      "class_id"
+    )}&risk_status=all`;
+    return apiClient("get", url);
+  }
+  notKnowledge() {
+    if (!this.state.tableKnowledge) {
+      return "";
+    } else {
+      return "Data belum tersedia.";
+    }
   }
   getSkill() {
-    let url = `v1/scores/report?semester=${this.state.selectedSemester.label}&category=skill&class_id=${localStorage.getItem("class_id")}&risk_status=${this.state.selectedStatus.value}`;
+    let url = `v1/scores/report?semester=${
+      this.state.selectedSemester.label
+    }&category=skill&class_id=${localStorage.getItem("class_id")}&risk_status=${
+      this.state.selectedStatus.value
+    }`;
     return apiClient("get", url);
   }
   getMountSkill() {
-    let url = `v1/scores/report?semester=${this.state.semesterActive}&category=skill&class_id=${localStorage.getItem("class_id")}&risk_status=all`;
-    return apiClient('get', url)
+    let url = `v1/scores/report?semester=${
+      this.state.semesterActive
+    }&category=skill&class_id=${localStorage.getItem(
+      "class_id"
+    )}&risk_status=all`;
+    return apiClient("get", url);
   }
   getAttitude() {
-    let url = `v1/scores/report?semester=${this.state.selectedSemester.label}&category=attitude&class_id=${localStorage.getItem("class_id")}&risk_status=${this.state.selectedStatus.value}`;
+    let url = `v1/scores/report?semester=${
+      this.state.selectedSemester.label
+    }&category=attitude&class_id=${localStorage.getItem(
+      "class_id"
+    )}&risk_status=${this.state.selectedStatus.value}`;
     return apiClient("get", url);
   }
   getMountAttitude() {
-    let url = `v1/scores/report?semester=${this.state.semesterActive}&category=attitude&class_id=${localStorage.getItem("class_id")}&risk_status=all`;
-    return apiClient('get', url)
+    let url = `v1/scores/report?semester=${
+      this.state.semesterActive
+    }&category=attitude&class_id=${localStorage.getItem(
+      "class_id"
+    )}&risk_status=all`;
+    return apiClient("get", url);
   }
   getMountData() {
     let tableKnowledge = [];
@@ -151,10 +184,10 @@ export default class Rapor extends Component {
             dTableKnowledge: iTableKnowledge,
             dTableAttitude: iTableAttitude,
             dTableSkill: iTableSkill
-          })
-        })
-      })
-    })
+          });
+        });
+      });
+    });
   }
   handleSubmit() {
     let tableKnowledge = [];
@@ -176,14 +209,14 @@ export default class Rapor extends Component {
             dTableKnowledge: iTableKnowledge,
             dTableAttitude: iTableAttitude,
             dTableSkill: iTableSkill
-          })
-        })
-      })
-    })
+          });
+        });
+      });
+    });
   }
   nameClicked(e, id) {
-    e.preventDefault()
-    this.props.history.push('detail/' + id);
+    e.preventDefault();
+    this.props.history.push("detail/" + id);
   }
   render() {
     return (
@@ -207,24 +240,47 @@ export default class Rapor extends Component {
               <div className="row margin-bottom-4">
                 <div className="col-4">
                   <h5 className="float-left margin-left-1 padding-top-1">
-                    <strong className="large-text">Rapor Kelas {this.state.nameClass}</strong>
+                    <strong className="large-text">
+                      Rapor Kelas {this.state.nameClass}
+                    </strong>
                   </h5>
                 </div>
                 <div className="col-8">
                   <span className="float-right margin-right-1">
                     <Nav tabs className="border-0 pull-right">
                       <NavItem className="tab-nilai">
-                        <NavLink className={classnames({ active: this.state.activeTab === "1" })} onClick={() => { this.toggle("1"); }}>
+                        <NavLink
+                          className={classnames({
+                            active: this.state.activeTab === "1"
+                          })}
+                          onClick={() => {
+                            this.toggle("1");
+                          }}
+                        >
                           Pengetahuan
                         </NavLink>
                       </NavItem>
                       <NavItem className="tab-nilai">
-                        <NavLink className={classnames({ active: this.state.activeTab === "2" })} onClick={() => { this.toggle("2"); }}>
+                        <NavLink
+                          className={classnames({
+                            active: this.state.activeTab === "2"
+                          })}
+                          onClick={() => {
+                            this.toggle("2");
+                          }}
+                        >
                           Keterampilan
                         </NavLink>
                       </NavItem>
                       <NavItem className="tab-nilai">
-                        <NavLink className={classnames({ active: this.state.activeTab === "3" })} onClick={() => { this.toggle("3"); }}>
+                        <NavLink
+                          className={classnames({
+                            active: this.state.activeTab === "3"
+                          })}
+                          onClick={() => {
+                            this.toggle("3");
+                          }}
+                        >
                           Sikap
                         </NavLink>
                       </NavItem>
@@ -232,13 +288,21 @@ export default class Rapor extends Component {
                   </span>
                 </div>
               </div>
-              <TabContent activeTab={this.state.activeTab} className="margin-left-1 margin-right-1">
+              <TabContent
+                activeTab={this.state.activeTab}
+                className="margin-left-1 margin-right-1"
+              >
                 <TabPane tabId="1">
-                  <TablePengetahuan
+                  {!this.state.tableKnowledge ||
+                  this.state.tableKnowledge.length === 0 ? (
+                    <NotAvailable>{this.notKnowledge()}</NotAvailable>
+                  ) : (
+                    <TablePengetahuan
                     tableKnowledge={this.state.tableKnowledge}
                     dTableKnowledge={this.state.dTableKnowledge}
                     nameClicked={this.nameClicked}
-                  />
+                    />
+                  )}
                 </TabPane>
                 <TabPane tabId="2">
                   <TableKeterampilan
