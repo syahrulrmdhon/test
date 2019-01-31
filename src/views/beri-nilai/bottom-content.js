54e89cac58654525a773a5f9942d86ae39b61ec8
hengkyreza7 <hengkyreza7@gmail.com>
2019-01-31 11:31:39 +0700

    -

--- src/views/beri-nilai/bottom-content.js
+++ src/views/beri-nilai/bottom-content.js
@@ -1,38 +1,66 @@
 import React, { Component } from 'react'
 import './../../styles/global/component.css'
 import Select from 'react-select'
+import {
+  Badge,
+  Button,
+  ButtonDropdown,
+  ButtonGroup,
+  ButtonToolbar,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Col,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Progress,
+  Row,
+  Table,
+} from 'reactstrap';
 export default class componentName extends Component {
-    render() {
-        return (
-            <div className="bottom-contents">
-                <div className="row  title-content">
-                    <div className="col-sm-6 ">
-                        <span>Hasil Perolehan Nilai</span>
-                    </div>
-                    <div className="col-sm-3">
-                        <Select
-                            classNamePrefix='select'
-                            placeholder='Urutkan Berdasarkan' />
-                    </div>
-                    <div className="col-sm-3">
-                        <div className="search-in-table">
-                            <input className="input-field" type="text" placeholder="Cari murid disini..." name="search" />
-                            <i className="fa fa-search icon"></i>
-                        </div>
-                    </div>
-
-
-                </div>
-                <div className="row">
-                <div className="table-responsive">
-                        <table className="table-nilai ">
-                            <tr>
-                                 <th>Nama Murid</th>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="bottom-contents">
+        <div className="row  title-content">
+          <div className="col-sm-6 ">
+            <span>Hasil Perolehan Nilai</span>
+          </div>
+          <div className="col-sm-3">
+            <Select
+              classNamePrefix='select'
+              placeholder='Urutkan Berdasarkan' />
+          </div>
+          <div className="col-sm-3">
+            <div className="search-in-table">
+              <input className="input-field" type="text" placeholder="Cari murid disini..." name="search" />
+              <i className="fa fa-search icon"></i>
             </div>
-        )
-    }
+          </div>
+
+
+        </div>
+        <div className="row">
+            <Table hover responsive className="table-outline mb-0 d-none d-sm-table margin-table-nilai">
+              <thead className="thead-light">
+                <tr>
+                  <th className="text-center"><i className="icon-people"></i></th>
+                  <th>User</th>
+                  <th className="text-center">Country</th>
+                  <th>Usage</th>
+                  <th className="text-center">Payment Method</th>
+                  <th>Activity</th>
+                </tr>
+              </thead>
+              
+            </Table>
+          </div>
+      </div>
+    )
+  }
 }
+
+
