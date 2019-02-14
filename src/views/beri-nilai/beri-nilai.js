0708d903010f15988901ef23cc2ae9ffeb9be3d1
hengky2 <hengky@gredu.asia>
2019-02-14 11:51:26 +0700

    filter participant

--- src/views/beri-nilai/beri-nilai.js
+++ src/views/beri-nilai/beri-nilai.js
@@ -10,7 +10,11 @@ import BottomContentEvaluasi from './evaluasi/bottom-content-evaluasi'
 import { apiClient } from '../../utils/apiClient'
 import RightContent from './right-content'
 import SubjectEvaluasi from './evaluasi/subject-evaluasi';
-export default class Nilai extends Component {
+import { getParticipant } from './../../redux-modules/modules/score'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+class Nilai extends Component {
   constructor(props) {
     super(props)
 
@@ -19,7 +23,7 @@ export default class Nilai extends Component {
       score: [],
       activeMenu: 1,
       exam: {},
-      exam_id:props.match.params.exam_id,
+      exam_id: props.match.params.exam_id,
       class_id: props.match.params.class_id,
       assessment_id: props.match.params.assessment_id,
       participant_passed: {},
@@ -28,12 +32,15 @@ export default class Nilai extends Component {
       competencySubjects: [],
       questionResults: [],
       examChart: [],
-      collapce:''
+      collapce: '',
+      search: ''
     }
     this.toggleMenu = this.toggleMenu.bind(this)
     this.fetchData = this.fetchData.bind(this)
     this.toggleMenu = this.toggleMenu.bind(this)
     this.handleNewScore = this.handleNewScore.bind(this)
+    this.onChangeSelect = this.onChangeSelect.bind(this)
+    this.onSubmmit = this.onSubmmit.bind(this)
   }
   componentDidMount() {
     console.log("here exam id", this.state.exam_id + '+' + this.state.class_id + '+' + this.state.assessment_id)
@@ -96,29 +103,28 @@ export default class Nilai extends Component {
 
     })
   }
+  onChangeSelect(e) {
+    this.setState({
+      search: e.target.value
+    })
+  }
 
 
-  handleNewScore(e, student){
+  handleNewScore(e, student) {
     e.preventDefault()
-    // let collapce = this.state.collapce
-    // if(collapce === ''){
-    //     this.setState({
-    //         collapce:'collapce'
-    //     })
-    // }else{
-    //     this.setState({
-    //         collapce:''
-    //     })
-    // }
     let data = 'ac67857a-ad71-4a97-9718-c71c47e2e4bc'
-
     this.props.history.push({
-        pathname:'/assessment/'+this.state.assessment_id+'/exam/'+this.state.exam_id+'/class/'+this.state.class_id+'/student/'+student,
-        state: {data:data}
+      pathname: '/assessment/' + this.state.assessment_id + '/exam/' + this.state.exam_id + '/class/' + this.state.class_id + '/student/' + student,
+      state: { data: data }
     })
 
 
-}
+  }
+
+  onSubmmit(){
+    console.log(this.state.search,"search")
+    this.props.getParticipant(this.state.exam_id, this.state.class_id, this.state.asssessment_id, this.state.search )
+  }
 
   render() {
     const tabMenu = ['Perolehan Nilai', 'Evaluasi Soal'];
@@ -153,11 +159,14 @@ export default class Nilai extends Component {
               <div className="row margin-top-4 padding-bottom-6 ">
                 <div className="col-sm-9">
                   <div className="content-block main-block">
-                    <BottomContent 
-                      exam = {this.state.exam_id}
-                      class = {this.state.class_id}
-                      asssessment = {this.state.assessment_id}
-                      handleNewScoreParent = {this.handleNewScore}
+                    <BottomContent
+                      exam={this.state.exam_id}
+                      class={this.state.class_id}
+                      asssessment={this.state.assessment_id}
+                      handleNewScoreParent={this.handleNewScore}
+                      search={this.state.search}
+                      onChange={this.onChangeSelect}
+                      submit={this.onSubmmit}
                     />
                   </div>
                 </div>
@@ -212,4 +221,11 @@ export default class Nilai extends Component {
   }
 }
 
+
+const mapStateToProps = state => ({
+  user: state.score
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({ getParticipant }, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(Nilai);
 
