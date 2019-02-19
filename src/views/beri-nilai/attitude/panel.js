c172eb4cf193941d5cf32240d630fb9779305065
hengkyreza7 <hengkyreza7@gmail.com>
2019-02-19 15:08:47 +0700

    switch

--- src/views/beri-nilai/attitude/panel.js
+++ src/views/beri-nilai/attitude/panel.js
@@ -12,16 +12,185 @@ export class panel extends Component {
                         <div className="col-sm-12">
                             <div className="col-sm-1 padding-4">
                                 <Avatar src={Ava} size={40} round={true} />
-                            </div> 
-                            <div className="col-sm-6 padding-4">
-                                <Avatar src={Ava} size={40} round={true} />
-                            </div>  
+                            </div>
+                            <div className="col-sm-5 padding-2">
+                                <div className="col-sm-12">
+                                    <span className="score-attitude__name">T Ritika Singh</span>
+                                </div>
+                                <div className="col-sm-12">
+                                    <div className="row">
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__predicate">Sikap Sangat Baik</span>
+                                        </div>
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__count-predicate-good">5 </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-sm-12">
+                                    <div className="row">
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__predicate">Sikap Baik</span>
+                                        </div>
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__count-predicate-enough">5 </span>
+                                        </div>  
+                                    </div>
+                                </div>
+                                <div className="col-sm-12">
+                                    <div className="row">
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__predicate">Sikap Butuh Perhatian</span>
+                                        </div>
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__count-predicate-need-attention">5 </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-2 padding-3">
+                                <div className="score-box border-full padding-top-2 padding-left-4 ">
+                                    <span className="score-attitude__score ">N/A</span>
+                                </div>
+                            </div>
+                            <div className="col-sm-4">
+                                <div className="row padding-top-3 padding-bottom-3">
+                                    <div className="padding-2 line-desc ">
+                                        <div className="col-sm-12">
+                                                <span className="score-attitude__desc-title">Diskripsi Sikap</span>
+                                        </div>
+                                        <div className="col-sm-12">
+                                            <span n className="score-attitude__desc-body">Deskripsi belum di masukan</span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="row margin-top-4">
                     <div className="panel margin-side-3">
-                        asdasdas
+                        <div className="col-sm-12">
+                            <div className="col-sm-1 padding-4">
+                                <Avatar src={Ava} size={40} round={true} />
+                            </div>
+                            <div className="col-sm-5 padding-2">
+                                <div className="col-sm-12">
+                                    <span className="score-attitude__name">T Ritika Singh</span>
+                                </div>
+                                <div className="col-sm-12">
+                                    <div className="row">
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__predicate">Sikap Sangat Baik</span>
+                                        </div>
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__count-predicate-good">5 </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-sm-12">
+                                    <div className="row">
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__predicate">Sikap Baik</span>
+                                        </div>
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__count-predicate-enough">5 </span>
+                                        </div>  
+                                    </div>
+                                </div>
+                                <div className="col-sm-12">
+                                    <div className="row">
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__predicate">Sikap Butuh Perhatian</span>
+                                        </div>
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__count-predicate-need-attention">5 </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-2 padding-3">
+                                <div className="score-box border-full padding-top-2 padding-left-4 ">
+                                    <span className="score-attitude__score ">N/A</span>
+                                </div>
+                            </div>
+                            <div className="col-sm-4">
+                                <div className="row padding-top-3 padding-bottom-3">
+                                    <div className="padding-2 line-desc ">
+                                        <div className="col-sm-12">
+                                                <span className="score-attitude__desc-title">Diskripsi Sikap</span>
+                                        </div>
+                                        <div className="col-sm-12">
+                                            <span n className="score-attitude__desc-body">Deskripsi belum di masukan</span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row margin-top-4">
+                    <div className="panel margin-side-3">
+                        <div className="col-sm-12">
+                            <div className="col-sm-1 padding-4">
+                                <Avatar src={Ava} size={40} round={true} />
+                            </div>
+                            <div className="col-sm-5 padding-2">
+                                <div className="col-sm-12">
+                                    <span className="score-attitude__name">T Ritika Singh</span>
+                                </div>
+                                <div className="col-sm-12">
+                                    <div className="row">
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__predicate">Sikap Sangat Baik</span>
+                                        </div>
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__count-predicate-good">5 </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-sm-12">
+                                    <div className="row">
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__predicate">Sikap Baik</span>
+                                        </div>
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__count-predicate-enough">5 </span>
+                                        </div>  
+                                    </div>
+                                </div>
+                                <div className="col-sm-12">
+                                    <div className="row">
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__predicate">Sikap Butuh Perhatian</span>
+                                        </div>
+                                        <div className="col-sm-5">
+                                            <span className="score-attitude__count-predicate-need-attention">5 </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-2 padding-3">
+                                <div className="score-box border-full padding-top-2 padding-left-4 ">
+                                    <span className="score-attitude__score ">N/A</span>
+                                </div>
+                            </div>
+                            <div className="col-sm-4">
+                                <div className="row padding-top-3 padding-bottom-3">
+                                    <div className="padding-2 line-desc ">
+                                        <div className="col-sm-12">
+                                                <span className="score-attitude__desc-title">Diskripsi Sikap</span>
+                                        </div>
+                                        <div className="col-sm-12">
+                                            <span n className="score-attitude__desc-body">Deskripsi belum di masukan</span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
 
