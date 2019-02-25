3374beb893362e799393e5420bfa41481cd522cc
hengky <hengky@gredu.asia>
2019-02-25 16:44:09 +0700

    fixing

--- src/views/beri-nilai/beri-nilai.js
+++ src/views/beri-nilai/beri-nilai.js
@@ -109,9 +109,9 @@ class Nilai extends Component {
   }
 
   onChangeScore(valueOpt){
+    console.log("here hitted",valueOpt)
+    this.props.getParticipant(this.state.exam_id, this.state.class_id, this.state.assessment_id, '', valueOpt)
     this.setState({ valueOpt })
-    let data = valueOpt.value ? valueOpt.value:''
-    this.props.getParticipant(this.state.exam_id, this.state.class_id, this.state.assessment_id, '', data)
   }
 
   handleNewScore(e, student) {
