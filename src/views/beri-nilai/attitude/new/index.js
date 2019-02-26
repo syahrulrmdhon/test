0f5188a94e9b42ea2ddc63d13ee3e6e9b9b256c1
hengky <hengky@gredu.asia>
2019-02-26 19:00:41 +0700

    attitude

--- src/views/beri-nilai/attitude/new/index.js
+++ src/views/beri-nilai/attitude/new/index.js
@@ -3,8 +3,9 @@ import { connect } from 'react-redux'
 import Title from './../../../../components/Title'
 import Header from './../../../global/header'
 import Report from './report-list'
+import Select from 'react-select'
 
-//import style
+//css
 import './../../../../styles/attitude.scss'
 
 export class componentName extends Component {
@@ -17,11 +18,50 @@ export class componentName extends Component {
                         <div className="margin-side-4 margin-top-7">
                             <div className="col-sm-12">
                                 <div className="col-sm-4" >
-                                    <div className="content-block main-block padding-top-2  ">
-                                        {/* <Report /> */}
+                                    <div className="content-block  content-score  ">
+                                        <Report />
                                     </div>
                                 </div>
-                               
+                                <div className="col-sm-8" >
+                                    <div className="content-block  content-score  ">
+                                        <div className="padding-3">
+                                            <div className="score-attitude-new__predicate-title">
+                                                <span className="score-attitude-new__right-title-name ">Nilai Ahkir(Disiplin)</span>
+                                                <span className="score-attitude-new__predicate-title"> - Hengky Reza Permana</span>
+                                            </div>
+                                            <div className="form margin-top-5">
+                                                <div className="row">
+                                                    <div className="col-sm-12">
+                                                        <label className="score-attitude-new__label-form">Nilai Ahkir</label>
+                                                        <div className="padding-top-1">
+                                                            <Select
+                                                                className="select-box"
+                                                                classNamePrefix="select"
+                                                                placeholder="Pilih Nilai Ahkir"
+                                                            />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="row padding-top-5">
+                                                    <div className="col-sm-12">
+                                                        <label className="score-attitude-new__label-form">Deskripsi Sikap</label>
+                                                        <div className="padding-top-1">
+                                                            <textarea className="textarea-description form-control" />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="row margin-top-10">
+                                                    <div className="col-sm-4">
+                                                        <div className="range-button ">
+                                                            <button className="btn-green">Submit</button>
+                                                        </div>  
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                     </div>
--- src/views/beri-nilai/attitude/new/report-list.js
+++ src/views/beri-nilai/attitude/new/report-list.js
@@ -11,39 +11,36 @@ export class Report extends Component {
         return (
             <div>
                 <div className="padding-4">
-                <div className="col-sm-12">
-                    <span className="score-attitude-new__left-title">Daftar Laporan Sikap (Matematika) </span>
-                </div>
-                <div className="col-sm-12">
-                    <span className="score-attitude-new__predicate-title">Semua sikap </span>
-                </div>
-                <div className="box-report">
+                    <div className="col-sm-12">
+                        <span className="score-attitude-new__left-title">Daftar Laporan Sikap (Matematika) </span>
+                    </div>
+                    <div className="col-sm-12">
+                        <span className="score-attitude-new__predicate-title">Semua sikap </span>
+                    </div>
+                    <div className="box-report">
 
+                    </div>
                 </div>
-            </div>
-            <div className="row ">
-                 <div className="col-sm-12">
+                <div className="row ">
                     <div className="border-top-score">
-                      <div className="margin-top-3 margin-left-3 ">   
-                        <NavLink className="font-grey  padding-3" to="/#" activeClassName="tab-predicate">
-                            Semua
+                        <div className="margin-top-3 margin-left-3 ">
+                            <NavLink className="font-grey  padding-3" to="/#" activeClassName="tab-predicate">
+                                Semua
                         </NavLink>
-                        <NavLink className="font-grey  padding-3" to="#" activeClassName="active-menu">
-                            Sangat Baik
+                            <NavLink className="font-grey  padding-3" to="#" activeClassName="active-menu">
+                                Sangat Baik
                         </NavLink>
-                        <NavLink className="font-grey  padding-3" to="#" activeClassName="active-menu">
-                            Baik
+                            <NavLink className="font-grey  padding-3" to="#" activeClassName="active-menu">
+                                Baik
                         </NavLink>
-                        <NavLink className="font-grey  padding-3" to="#" activeClassName="active-menu">
-                            Butuh Perhatian
+                            <NavLink className="font-grey  padding-3" to="#" activeClassName="active-menu">
+                                Butuh Perhatian
                         </NavLink>
-                    
-                     </div>
-                     </div>
 
-                 </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-         </div>
         )
     }
 }
