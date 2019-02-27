1e33b7dbbcc633880e8f860f7dc08054214bf8ab
Fandi A Riyanto <fandi@gredu.asia>
2019-02-27 16:27:07 +0700

    update responsive page daftar-nilai & rapor

--- src/views/daftar-nilai/daftar-nilai.js
+++ src/views/daftar-nilai/daftar-nilai.js
@@ -170,7 +170,7 @@ export default class DaftarNilai extends Component {
           <Header />
           <div className='content'>
             <div className='row row-score'>
-              <div className='left-content col-sm-2'>
+              <div className='left-content col-sm-2 col-lg-2'>
                 <FilterNilai
                   id='mySelect'
                   listClass={this.state.listClass}
@@ -185,14 +185,14 @@ export default class DaftarNilai extends Component {
                   handleSubmit={this.handleSubmit}
                 />
               </div>
-              <div className='right-content col-sm-10'>
+              <div className='right-content col-sm-10 col-lg-10'>
                 <div className='row margin-bottom-4'>
-                  <div className='col-sm-2'>
+                  <div className='col-sm-3 col-lg-2'>
                     <h5 className='float-left margin-left-1 padding-top-1'>
                       <strong className='large-text'>Daftar Nilai</strong>
                     </h5>
                   </div>
-                  <div className='col-sm-10'>
+                  <div className='col-sm-9 col-lg-10'>
                     <span className='float-right margin-right-1'>
                       <Nav tabs className='toggle border-0 pull-right tab-span'>
                         <NavItem className='tab-nilai'>
