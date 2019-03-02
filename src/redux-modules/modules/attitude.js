6fad8fc3398bd00aa6410c3fb1d362dd6439cc48
hengky <hengky@gredu.asia>
2019-03-01 20:39:01 -0800

    finish score attitude

--- src/redux-modules/modules/attitude.js
+++ src/redux-modules/modules/attitude.js
@@ -4,53 +4,48 @@ const LOAD = 'modules/attitude/LOAD';
 const LOAD_SUCCESS = 'modules/attitude/LOAD_SUCCESS';
 const LOAD_FAIL = 'modules/attitude/LOAD_FAIL';
 const LOGOUT = 'modules/attitude/LOGOUT';
-const LOAD_FORM_DATA =  'modules/attitude/LOAD_FORM_DATA' 
+const HANDLE_FORM_TEXT = 'modules/attitude/HANDLE_FORM_TEXT'
+const LOAD_FORM_DATA = 'modules/attitude/LOAD_FORM_DATA'
 const initialState = null;
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
 
         case LOAD_FORM_DATA:
-        delete state.error
-        if (state.result !== action.result) {
-            console.log(action)
-            // if (action.result.data.collections) {
-            //     action.result.data['exam_question'] = []
-            //     action.result.data.collections.map((collection) => {
-            //             action.result.data['exam_question'].push({
-            //                 ans: collection.user_problem_answer ? collection.user_problem_answer.ans:null ,
-            //                 score: collection.user_problem_answer ? collection.user_problem_answer.score:null,
-            //                 exam_question_id: collection.id,
-            //                 user_id: action.data.student ,
-            //                 exam_id: action.data.exam,
-            //                 class_id: action.data.classess,
-            //                 problem_type:collection.problem_type,
-            //                 user_problem_answer:collection.user_problem_answer,
-            //                 exam_question_choices: collection.exam_question_choices,
-            //                 qn_number: collection.qn_number,
-            //                 weight:collection.weight,
-            //                 max_score: collection.max_score
-            //             })
-                    
-            //     })
-            // }
-
+            delete state.error
+            if (state.result !== action.result) {
+                if (action.result.data.user_attitude) {
+                    action.result.data['data_form'] = {}
+                    action.result.data['data_form'] = {
+                        score: action.result.data.user_attitude.score,
+                        description: action.result.data.user_attitude.description
+                    }
+                }
+                return {
+                    ...state,
+                    loaded: true,
+                    loading: false,
+                    result: true,
+                    ...action.result
+                }
+            }
+            break;
+        case HANDLE_FORM_TEXT:
+            console.log(action, "actin")
+            state.data.data_form[action.field] = action.value
+            console.log(state.data.data_form, "actin")
             return {
                 ...state,
                 loaded: true,
                 loading: false,
-                result: true,
-                ...action.result
             }
-        }
-        break;
         case LOAD:
             return {
                 ...state,
                 loaded: false,
                 loading: true,
             }
-        
+
         case LOAD_SUCCESS:
             delete state.error;
             console.log(action, "my result")
@@ -75,11 +70,30 @@ export default function reducer(state = initialState, action) {
         case LOGOUT:
             return { ...initialState }
         case SET:
-            return { ...state, loaded: true, loading: false, result: true, data: { ...state.data, ...action.payload } }
+            return { ...state, loaded: true, loading: false, result: true, data: { ...state, ...action } }
         case RESET:
             return { ...initialState }
-        default:
-            return state
+        default: {
+            return { ...state }
+        }
+    }
+}
+
+export function handlingInputText(e, field_name) {
+    console.log(e.target.value, "value")
+    return {
+        type: HANDLE_FORM_TEXT,
+        value: e.target.value,
+        field: field_name
+    }
+}
+
+export function handlingInputSelect(e, field_name) {
+    console.log(e.value, "value")
+    return {
+        type: HANDLE_FORM_TEXT,
+        value: e.value,
+        field: field_name
     }
 }
 
@@ -101,13 +115,13 @@ export function getDataScoreAttitude(assessment_id) {
 }
 
 
-export function getDataScoreDetail(assessment_id, class_id, user_id) {
+export function getDataScoreDetail(assessment_id, class_id, user_id,tipe) {
     const token = localStorage.getItem('token')
     const schoolId = localStorage.getItem("school_id")
-    const url = `${process.env.API_URL}v1/assessments/${assessment_id}/classes/${class_id}/users/${user_id}?attitudes_scores=0?page=2`
-    console.log(url,"my url")
+    const url = `${process.env.API_URL}v1/assessments/${assessment_id}/classes/${class_id}/users/${user_id}?page=1&attitudes_scores=${tipe}`
+    console.log(url, "my url")
     return {
-        types: [LOAD, LOAD_SUCCESS,LOAD_FORM_DATA, LOAD_FAIL],
+        types: [LOAD, LOAD_FORM_DATA, LOAD_FAIL],
         promise: client => client.get(url, {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
