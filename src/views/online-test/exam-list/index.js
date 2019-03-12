371aa94cad1a2804ba9798c472ed45f3f7d669fd
Fandi A Riyanto <fandi@gredu.asia>
2019-03-12 18:15:11 +0700

    layout index ujian online

---
+++ src/views/online-test/exam-list/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import Header from './../../global/header'
+import Page from './../../../components/Title'
+import FilterOnlineExam from './filter'
+import './../../../styles/online-test.scss'
+import { NotAvailable } from '../../global/notAvailable'
+import { Progress } from 'reactstrap'
+
+export default class OnlineExamList extends Component {
+    render() {
+        return (
+            <Page title='Daftar Ujian'>
+                <div className='online-exam'>
+                    <div className='padding-content'>
+                        <Header />
+                        <div className='margin-content'>
+                            <div className='content-block main-block'>
+                                <div className='margin-0'>
+                                    <div className='col-sm-3 left-block'>
+                                        <FilterOnlineExam
+
+                                        />
+                                    </div>
+                                    <div className='col-sm-9 right-block w-100'>
+                                        <div className='header-content'>
+                                            <label className='header-title disblock padding-top-6'>Daftar Ujian</label>
+                                            <label className='padding-top-2 info margin-bottom-3'>Ujian Tengah Semester [Periode]</label>
+                                        </div>
+                                        <div className='padding-top-4 disblock right-block__panel-wrapper'>
+                                            <div className='right-block__panel'>
+                                                <div className='row h-100'>
+                                                    <div className='col-sm-4 right-block__panel-part'>
+                                                        <label className='header-title disblock'>Daftar Ujian</label>
+                                                        <label className='padding-top-2 info margin-bottom-3'>Ujian Tengah Semester [Periode]</label>
+                                                    </div>
+                                                    <div className='col-sm-4 right-block__panel-part'>
+                                                        <Progress value={75} color="success" animated />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            {/* <NotAvailable>Ujian belum tersedia</NotAvailable> */}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Page>
+        )
+    }
+}
