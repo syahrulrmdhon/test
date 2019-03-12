78ad260f31a7c43b4f457e63b06701397c3abd0d
Fandi A Riyanto <fandi@gredu.asia>
2019-03-12 12:42:03 +0700

    fixing bug UI at score list

--- src/views/daftar-nilai/tab.js
+++ src/views/daftar-nilai/tab.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { TabContent, TabPane } from 'reactstrap'
-import {NotAvailable} from './../global/notAvailable'
+import { NotAvailable } from './../global/notAvailable'
 import TablePengetahuan from './table-pengetahuan'
 import TableKeterampilan from './table-keterampilan'
 import TableSikap from './table-sikap'
@@ -8,46 +8,44 @@ import TableSikap from './table-sikap'
 export default class Tab extends Component {
     render() {
         return (
-            <div className='margin-left-1 margin-right-1'>
-                <TabContent className='tab-content-score' activeTab={this.props.activeTab}>
-                    <TabPane tabId='1'>
-                        {!this.props.tableKnowledge ||
-                            this.props.tableKnowledge.length === 0 ? (
-                                <NotAvailable>Mohon pilih semua filter untuk menampilkan data.</NotAvailable>
-                            ) : (
-                                <TablePengetahuan
-                                    tableKnowledge={this.props.tableKnowledge}
-                                    idxScores={this.props.idxScores}
-                                    idxTugas={this.props.idxTugas}
-                                    nameClicked={this.props.nameClicked}
-                                />
-                            )}
-                    </TabPane>
-                    <TabPane tabId='2'>
-                        {!this.props.tableSkill ||
-                            this.props.tableSkill.length === 0 ? (
-                                <NotAvailable>Mohon pilih semua filter untuk menampilkan data.</NotAvailable>
-                            ) : (
-                                <TableKeterampilan
-                                    tableSkill={this.props.tableSkill}
-                                    idxScoresSkill={this.props.idxScoresSkill}
-                                    nameClicked={this.props.nameClicked}
-                                />
-                            )}
-                    </TabPane>
-                    <TabPane tabId='3'>
-                        {!this.props.tableAttitude ||
-                            this.props.tableAttitude.length === 0 ? (
-                                <NotAvailable>Mohon pilih semua filter untuk menampilkan data.</NotAvailable>
-                            ) : (
-                                <TableSikap
-                                    tableAttitude={this.props.tableAttitude}
-                                    nameClicked={this.props.nameClicked}
-                                />
-                            )}
-                    </TabPane>
-                </TabContent>
-            </div>
+            <TabContent className='tab-content-score margin-left-1 margin-right-1' activeTab={this.props.activeTab}>
+                <TabPane tabId='1'>
+                    {!this.props.tableKnowledge ||
+                        this.props.tableKnowledge.length === 0 ? (
+                            <NotAvailable>Mohon pilih semua filter untuk menampilkan data.</NotAvailable>
+                        ) : (
+                            <TablePengetahuan
+                                tableKnowledge={this.props.tableKnowledge}
+                                idxScores={this.props.idxScores}
+                                idxTugas={this.props.idxTugas}
+                                nameClicked={this.props.nameClicked}
+                            />
+                        )}
+                </TabPane>
+                <TabPane tabId='2'>
+                    {!this.props.tableSkill ||
+                        this.props.tableSkill.length === 0 ? (
+                            <NotAvailable>Mohon pilih semua filter untuk menampilkan data.</NotAvailable>
+                        ) : (
+                            <TableKeterampilan
+                                tableSkill={this.props.tableSkill}
+                                idxScoresSkill={this.props.idxScoresSkill}
+                                nameClicked={this.props.nameClicked}
+                            />
+                        )}
+                </TabPane>
+                <TabPane tabId='3'>
+                    {!this.props.tableAttitude ||
+                        this.props.tableAttitude.length === 0 ? (
+                            <NotAvailable>Mohon pilih semua filter untuk menampilkan data.</NotAvailable>
+                        ) : (
+                            <TableSikap
+                                tableAttitude={this.props.tableAttitude}
+                                nameClicked={this.props.nameClicked}
+                            />
+                        )}
+                </TabPane>
+            </TabContent>
         )
     }
 }
