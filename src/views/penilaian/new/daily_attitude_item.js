1cd4a24492c3c4d836020672bf3d1794286ecb8a
hengky <hengky@gredu.asia>
2019-03-20 00:56:42 -0700

    -

--- src/views/penilaian/new/daily_attitude_item.js
+++ src/views/penilaian/new/daily_attitude_item.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Select from 'react-select';
-import Autocomplete from 'react-autocomplete';
 
 import { apiClient } from './../../../utils/apiClient'
 import {
