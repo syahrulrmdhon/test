9871381c991587328617086fb15e76247cc59cb6
Fandi A Riyanto <fandi@gredu.asia>
2019-03-21 11:08:40 +0700

    update implement not available at list ujian online

--- src/views/online-test/exam-list/content.js
+++ src/views/online-test/exam-list/content.js
@@ -18,61 +18,63 @@ class ContentOnlineExam extends Component {
         let content = []
 
         if (entries) {
-            entries.map((x) => {
-                content.push(
-                    <div className='disblock' key={Math.random()}>
-                        <div className='right-block__panel'>
-                            <div className='row h-100'>
-                                <div className='col-sm-4 right-block__panel-part'>
-                                    <label className='header-title disblock padding-bottom-1'>{x.name}</label>
-                                    <div className='right-block__basic-info-wrapper'>
-                                        <div className='right-block__basic-info'>
-                                            <i className='fa fa-calendar-o padding-right-1'></i>
-                                            <label className='info'>{x.created_date}</label>
+            if (entries.length) {
+                entries.map((x) => {
+                    content.push(
+                        <div className='disblock' key={Math.random()}>
+                            <div className='right-block__panel'>
+                                <div className='row h-100'>
+                                    <div className='col-sm-4 right-block__panel-part'>
+                                        <label className='header-title disblock padding-bottom-1'>{x.name}</label>
+                                        <div className='right-block__basic-info-wrapper'>
+                                            <div className='right-block__basic-info'>
+                                                <i className='fa fa-calendar-o padding-right-1'></i>
+                                                <label className='info'>{x.created_date}</label>
+                                            </div>
+                                            <div className='right-block__basic-info'>
+                                                {x.assessment_type_text === null ? '' : x.assessment_type_text}
+                                            </div>
+                                            <div className='right-block__basic-info'>
+                                                {x.duration !== null ? '0' : x.duration} Menit
+                                             </div>
                                         </div>
-                                        <div className='right-block__basic-info'>
-                                            {x.assessment_type_text === null ? '' : x.assessment_type_text}
+                                        <label className='p'>{x.grade_name === null ? 'Kelas Belum Dipilih' : x.grade_name}</label>
+                                        <div className='padding-top-2 right-block__action-wrapper'>
+                                            <div className='right-block__action'>Lihat</div>
+                                            <div className='right-block__action'>Ubah Soal</div>
+                                            <div className='right-block__action'>Hapus Soal</div>
                                         </div>
-                                        <div className='right-block__basic-info'>
-                                            {x.duration !== null ? '0' : x.duration} Menit
-                                         </div>
                                     </div>
-                                    <label className='p'>{x.grade_name === null ? 'Kelas Belum Dipilih' : x.grade_name}</label>
-                                    <div className='padding-top-2 right-block__action-wrapper'>
-                                        <div className='right-block__action'>Lihat</div>
-                                        <div className='right-block__action'>Ubah Soal</div>
-                                        <div className='right-block__action'>Hapus Soal</div>
+                                    <div className='col-sm-4 right-block__panel-part padding-top-4'>
+                                        <div className='padding-bottom-1'>
+                                            <label className='p'>Progress</label>
+                                            <span className='pull-right p-green'>{x.assigned_exam.percentage === null ? '0' : x.assigned_exam.percentage}%</span>
+                                        </div>
+                                        <Progress value={x.assigned_exam.progress} />
+                                        <label className='info padding-top-1'>{x.assigned_exam.created_question_count === null ? '0' : x.assigned_exam.created_question_count}/{x.assigned_exam.question_count === null ? '0' : x.assigned_exam.question_count} Terbuat</label>
                                     </div>
-                                </div>
-                                <div className='col-sm-4 right-block__panel-part padding-top-4'>
-                                    <div className='padding-bottom-1'>
-                                        <label className='p'>Progress</label>
-                                        <span className='pull-right p-green'>{x.assigned_exam.percentage === null ? '0' : x.assigned_exam.percentage}%</span>
-                                    </div>
-                                    <Progress value={x.assigned_exam.progress} />
-                                    <label className='info padding-top-1'>{x.assigned_exam.created_question_count === null ? '0' : x.assigned_exam.created_question_count}/{x.assigned_exam.question_count === null ? '0' : x.assigned_exam.question_count} Terbuat</label>
-                                </div>
-                                <div className='col-sm-4'>
-                                    <div className='padding-bottom-2'>
-                                        <label className='p-bold padding-right-2'>Detil Soal</label>
-                                        <i className='fa fa-pencil icon-green'></i>
-                                    </div>
-                                    <div className='padding-bottom-2'>
-                                        <DetailOnlineExam
-                                            detail={x.assigned_exam}
-                                        />
-                                        <label className='info text-justify'>{x.assigned_exam.description === null ? ' ' : x.assigned_exam.description}</label>
+                                    <div className='col-sm-4'>
+                                        <div className='padding-bottom-2'>
+                                            <label className='p-bold padding-right-2'>Detil Soal</label>
+                                            <i className='fa fa-pencil icon-green'></i>
+                                        </div>
+                                        <div className='padding-bottom-2'>
+                                            <DetailOnlineExam
+                                                detail={x.assigned_exam}
+                                            />
+                                            <label className='info text-justify'>{x.assigned_exam.description === null ? ' ' : x.assigned_exam.description}</label>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                )
-            })
-        } else {
-            <NotAvailable>Data Tidak Tersedia</NotAvailable>
+                    )
+                })
+            }
+            else {
+                content.push(<NotAvailable key={Math.random()}>Data Tidak Tersedia</NotAvailable>)
+            }
         }
-
 
         return (
             <div className='col-sm-9 right-block'>
@@ -88,7 +90,7 @@ class ContentOnlineExam extends Component {
                                 <Loader loader={true} />
                                 :
                                 content
-                                
+
                         }
                     </div>
                 </div>
