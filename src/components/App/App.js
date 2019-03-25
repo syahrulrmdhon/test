b1be697a358b6b7839c4578c0f3827756b11b50c
Syahrul Romadhon <syahrul@gredu.asia>
2019-03-25 11:28:45 +0700

    add safari class

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -35,6 +35,7 @@ class App extends Component {
     }
     
     this.disableScreen();
+    this.safari();
   }
 
   disableScreen () {
@@ -45,6 +46,12 @@ class App extends Component {
         message: `Resolusi layar tidak mendukung.`,
       })
     }
+}
+
+safari() {
+  if (window.safari) {
+    document.getElementsByTagName("BODY")[0].className += " safari";
+  }
 }
 
 // Initial execution if needed
