6019611f13627e94184f9be45327ed11fa58aaed
MIftahul Khoir <miftaahulkhoir@gmail.com>
2019-04-05 15:12:24 +0700

    update styling and delete filter question type in modal import question

--- src/views/online-test/bank/bank.js
+++ src/views/online-test/bank/bank.js
@@ -10,10 +10,6 @@ import _ from 'lodash'
 
 class Bank extends Component {
 
-    componentDidMount() {
-        this.props.getBank()
-    }
-
     openModal() {
         this.setState({
             visible: true
@@ -29,7 +25,7 @@ class Bank extends Component {
     render() {
         return (
             <section className='bank'>
-                <Modal visible={this.props.visible} width="60%" height="90%" effect="fadeInUp" onClickAway={() => this.props.closeModal()}>
+                <Modal visible={this.props.visible} width="664px" height="90%" effect="fadeInUp" onClickAway={() => this.props.closeModal()}>
                     <div className='wrapper-content margin-side-4'>
                         <div className='header margin-top-4 margin-bottom-2'>
                             <div className='header-title padding-bottom-1'>
